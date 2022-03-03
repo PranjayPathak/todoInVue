34896a7f9a789f6a1a4ff9b007239657f597bb67
pranjaypathak411 <pranjaypathak411@gmail.com>
2022-03-03 14:39:17 +0530

    Intitalized the structure and added design

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import TaskCell from "./components/TaskCell.vue";
+</script>
+
+<template>
+  <div class="container bg-primary-600 rounded-sm m-10 shadow-2xl">
+    <div class="p-4">
+      <div>
+        <p class="headline-2 my-5">Todo in Vue</p>
+        <form>
+          <div>
+            <!-- <label for="task-input" class="label-lg my-4 col">New Task:</label> -->
+            <input
+              id="task-input"
+              class="input input-lg mt-3 mb-5"
+              placeholder="Please add new task"
+              required="true"
+              type="text"
+            />
+            <button class="m-5 btn success p-5" type="submit">Add +</button>
+          </div>
+        </form>
+      </div>
+      <div class="py-10 px-5">
+        <TaskCell />
+
+        <TaskCell />
+
+        <TaskCell />
+
+        <TaskCell />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
---
+++ src/components/TaskCell.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  task_info: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="p-3 my-3 row rounded-sm bg-primary-200">
+    <p class="task-info container body-lg col">Demo Task</p>
+    <button class="btn primary sm col-1">Done</button>
+
+    <button class="btn primary sm col-1">Delete</button>
+  </div>
+</template>
+
+<style scoped>
+.task-info {
+  color: #222222;
+}
+</style>
