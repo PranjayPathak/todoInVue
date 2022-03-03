7780c5b8e54244a88597a5f0c0f18a86f7edb21a
pranjaypathak411 <pranjaypathak411@gmail.com>
2022-03-03 18:49:10 +0530

    Add funtionality to the todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,72 @@
-<script setup>
+<script>
 import TaskCell from "./components/TaskCell.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      demoTasks: [
+        {
+          data: "demo task 1",
+          id: 0,
+        },
+        {
+          data: "demo task 2",
+          id: 1,
+        },
+        {
+          data: "demo task 3",
+          id: 2,
+        },
+        {
+          data: "demo task 4",
+          id: 3,
+        },
+      ],
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      this.demoTasks.unshift({ data: this.newTask, id: this.demoTasks.length });
+    },
+    deleteTask(taskId) {
+      let prevTasks = [...this.demoTasks];
+      let newDemoTasks = prevTasks.filter((task) => {
+        //filtering task that matches the description
+        return task.id !== taskId;
+      });
+      this.demoTasks = newDemoTasks;
+    },
+  },
+  components: { TaskCell },
+};
 </script>
 
 <template>
   <div class="container bg-primary-600 rounded-sm m-10 shadow-2xl">
-    <div class="p-4">
-      <div>
+    <div class="p-2">
+      <div class="py-5">
         <p class="headline-2 my-5">Todo in Vue</p>
-        <form>
+        <form @submit.prevent="addTask">
           <div>
-            <!-- <label for="task-input" class="label-lg my-4 col">New Task:</label> -->
             <input
               id="task-input"
               class="input input-lg mt-3 mb-5"
+              v-model="newTask"
               placeholder="Please add new task"
               required="true"
               type="text"
             />
-            <button class="m-5 btn success p-5" type="submit">Add +</button>
+            <button class="m-5 btn success p-5">Add +</button>
           </div>
         </form>
       </div>
-      <div class="py-10 px-5">
-        <TaskCell />
-
-        <TaskCell />
-
-        <TaskCell />
-
-        <TaskCell />
+      <div
+        class="py-1 px-5"
+        v-for="taskInfo in this.demoTasks"
+        :key="taskInfo.id"
+      >
+        <TaskCell :task-info="taskInfo" v-on:delTask="deleteTask($event)" />
       </div>
     </div>
   </div>
--- src/components/TaskCell.vue
+++ src/components/TaskCell.vue
@@ -1,18 +1,36 @@
-<script setup>
-defineProps({
-  task_info: {
-    type: String,
-    required: true,
+<script>
+export default {
+  data() {
+    return {
+      task: "",
+    };
   },
-});
+  props: {
+    taskInfo: {
+      type: Object,
+      default: "default task",
+      required: true,
+    },
+  },
+  methods: {
+    deleteTask() {
+      this.$emit("delTask", this.task.id);
+    },
+  },
+  mounted() {
+    this.task = this.taskInfo;
+  },
+};
 </script>
 
 <template>
-  <div class="p-3 my-3 row rounded-sm bg-primary-200">
-    <p class="task-info container body-lg col">Demo Task</p>
-    <button class="btn primary sm col-1">Done</button>
+  <div>
+    <div class="p-3 my-3 row rounded-sm bg-primary-200">
+      <p class="task-info container body-lg col">{{ this.task.data }}</p>
+      <!-- <button class="btn primary sm col-1">Done</button> -->
 
-    <button class="btn primary sm col-1">Delete</button>
+      <button class="btn primary sm col-1" @click="deleteTask">Delete</button>
+    </div>
   </div>
 </template>
 
@@ -20,4 +38,8 @@ defineProps({
 .task-info {
   color: #222222;
 }
+
+/* .strike {
+  text-decoration: line-through;
+} */
 </style>
